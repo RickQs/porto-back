beb142fe0282731adc83bd0e07afdee2c7df9a0c
Henrique Rinaldi <henriquergr@hotmail.com>
2023-06-13 10:14:42 -0300

    config: Adicionado config de banco

---
+++ PortoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace porto_back
+{
+    public class PortoDb : DbContext
+    {
+        public PortoDb(DbContextOptions<PortoDb> options)
+            : base(options) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using porto_back;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PortoDb>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
